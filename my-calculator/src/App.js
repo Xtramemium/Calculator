d3ccf9802e6d2b97f80dd8a0606b3f9ba107e9a8
Xtramemium <ivanteev85@gmail.com>
2023-05-17 15:52:54 +0300

    HTML created and some css changes

--- my-calculator/src/App.js
+++ my-calculator/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./CalcStyles.css"
+import styles from "./MyCalculator.module.css"
+const arr = [0,1,2,3,4,5,6,7,8,9]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+        <div className={styles.Container}>
+            <div className={styles.Output}>
+                <div className={styles.PreviousNumber}>previous number</div>
+                <div className={styles.CurrentNumber}>current number</div>
+            </div>
+            <button className={styles.ClearContent}>C</button>
+            <button>DEL</button>
+            <button>{arr[1]}</button>
+            <button>{arr[2]}</button>
+            <button>{arr[3]}</button>
+            <button>{arr[4]}</button>
+            <button>{arr[5]}</button>
+            <button>{arr[6]}</button>
+            <button>{arr[7]}</button>
+            <button>{arr[8]}</button>
+            <button>{arr[9]}</button>
+            <button className={styles.ZeroButton}>{arr[0]}</button>
+
+            <button>*</button>
+            <button>/</button>
+            <button>-</button>
+            <button>=</button>
+        </div>
     </div>
   );
 }
