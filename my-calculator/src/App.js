ff169b928a5ea538d12fd449028a2626f6922bd1
Xtramemium <ivanteev85@gmail.com>
2023-05-18 13:44:17 +0300

    Some css changes

--- my-calculator/src/App.js
+++ my-calculator/src/App.js
@@ -1,5 +1,5 @@
-import "./CalcStyles.css"
-import styles from "./MyCalculator.module.css"
+import "./general-calc-styles.css"
+import styles from "./my-calculator.module.css"
 const arr = [0,1,2,3,4,5,6,7,8,9]
 
 function App() {
@@ -11,22 +11,22 @@ function App() {
                 <div className={styles.CurrentNumber}>current number</div>
             </div>
             <button className={styles.ClearContent}>C</button>
-            <button>DEL</button>
-            <button>{arr[1]}</button>
-            <button>{arr[2]}</button>
-            <button>{arr[3]}</button>
-            <button>{arr[4]}</button>
-            <button>{arr[5]}</button>
-            <button>{arr[6]}</button>
-            <button>{arr[7]}</button>
-            <button>{arr[8]}</button>
-            <button>{arr[9]}</button>
+            <button className={styles.DelButton}>DEL</button>
+            <button className={styles.NumberButton}>{arr[1]}</button>
+            <button className={styles.NumberButton}>{arr[2]}</button>
+            <button className={styles.NumberButton}>{arr[3]}</button>
+            <button className={styles.NumberButton}>{arr[4]}</button>
+            <button className={styles.NumberButton}>{arr[5]}</button>
+            <button className={styles.NumberButton}>{arr[6]}</button>
+            <button className={styles.NumberButton}>{arr[7]}</button>
+            <button className={styles.NumberButton}>{arr[8]}</button>
+            <button className={styles.NumberButton}>{arr[9]}</button>
             <button className={styles.ZeroButton}>{arr[0]}</button>
-
-            <button>*</button>
-            <button>/</button>
-            <button>-</button>
-            <button>=</button>
+            <button className={styles.CalculateButton}>/</button>
+            <button className={styles.CalculateButton}>*</button>
+            <button className={styles.CalculateButton}>-</button>
+            <button className={styles.CalculateButton}>+</button>
+            <button className={styles.CalculateButton}>=</button>
         </div>
     </div>
   );
