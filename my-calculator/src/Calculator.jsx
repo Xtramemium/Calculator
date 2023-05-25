c3159f78de3378ef042c6526f08ab6631279c598
Xtramemium <ivanteev85@gmail.com>
2023-05-25 15:43:41 +0300

    calc created

---
+++ my-calculator/src/Calculator.jsx
@@ -0,0 +1,83 @@
+
+import { useState } from 'react';
+
+function App() {
+
+    const [butArr, setButArr] = useState(["1", '2', '3', '4', '5', '6', '7', '8', '9'])
+
+    const actions = ['+', '-', 'C', '=']
+
+    const [operator, setOperator] = useState('')
+    const [value, setValue] = useState('0')
+    const [value1, setValue1] = useState('')
+    const [finish, setFinish] = useState(false)
+
+    const result = value + operator + value1
+
+    const calculatePlus = () => {
+        return  Number(value + value1)
+    }
+
+    const calculateMinus = () => {
+        return value - value1
+    }
+
+
+    const handleClick = (val) => {
+        if (value === "0") {
+            setValue(val)
+        } else if (operator) {
+            setValue1((prev) => {
+                return prev + val
+            })
+        } else {
+            setValue((prev) => prev + val)
+        }
+    }
+
+
+    const handleOperation = (op) => {
+        if (op === 'C') {
+            setValue('0')
+            setValue1('')
+            setOperator('')
+        } else if (op === '=') {
+            if (operator === '+') {
+                console.log('Значения:',value, value1)
+                console.log('тип выражения:', typeof calculatePlus())
+                setValue(calculatePlus)
+                setValue1('')
+                setOperator('')
+            }
+            else if (op === '-') {
+                calculateMinus()
+            }
+            setFinish(true)
+        } else {
+            setOperator(op)
+        }
+
+    }
+
+    return (
+        <div className='App' >
+            <h1>{result}</h1>
+            <ul className={'numButtons'}>
+                {butArr.map((item, ind) => (
+                    <button className={'numberButtons'} key={ind} onClick={() => handleClick(item)}  >
+                        <div >
+                            {item}
+                        </div>
+                    </button>))}
+            </ul>
+            <ul className={'caclButtons'}>
+            {actions.map((act) => (
+                <button className={'calculateButtons'} key={act} onClick={() => handleOperation(act)}  >
+                    {act}
+                </button>))}
+            </ul>
+        </div>
+    );
+}
+
+export default App;
